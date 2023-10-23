2d4c560614f38e95872a6718259fae348e7c8f06
wandieinnocents <wandieinnocent2@gmail.com>
2023-10-23 23:44:03 +0300

    Add top bar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
+import { ColorModeContext,useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+
+// components
+import TopBar from "./scenes/global/TopBar";
+
+
 
 function App() {
-  return (
-    <div className="app">
-      
+
+  const [ theme, colorMode ] = useMode();
+
+  return ( 
+  <ColorModeContext.Provider value={colorMode}>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+
+
+    <div className="app"> 
+
+    {/* Main content */}
+    <main className="content">
+    <TopBar />
+
+
+    </main>
+
+    {/* End main content */}
+    
     </div>
+    </ThemeProvider>
+    
+    </ColorModeContext.Provider>
   );
 }
 
