551ce308e02c5745586d09d66df3c3659f9287ec
wandieinnocents <wandieinnocent2@gmail.com>
2023-10-24 00:02:09 +0300

    Add router ,and refactor other sections of the code

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
-import { ColorModeContext,useMode } from "./theme";
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
 import { CssBaseline, ThemeProvider } from "@mui/material";
+import { ColorModeContext, useMode } from "./theme";
 
-// components
 import TopBar from "./scenes/global/TopBar";
+import SideBar from "./scenes/global/SideBar";
+import Dashboard from "./scenes/dashboard";
+// import Team from "./scenes/team";
+// import Invoices from "./scenes/invoices";
+// import Contacts from "./scenes/contacts";
+// import Bar from "./scenes/bar";
+// import Form from "./scenes/form";
+// import Line from "./scenes/line";
+// import Pie from "./scenes/pie";
+// import FAQ from "./scenes/faq";
+// import Geography from "./scenes/geography";
+// import Calendar from "./scenes/calendar/calendar";
+
+
+
 
 
 
 function App() {
 
-  const [ theme, colorMode ] = useMode();
+  const [theme, colorMode] = useMode();
+  const [isSidebar, setIsSidebar] = useState(true);
 
   return ( 
   <ColorModeContext.Provider value={colorMode}>
@@ -18,9 +35,24 @@ function App() {
 
     <div className="app"> 
 
+    <SideBar />
     {/* Main content */}
     <main className="content">
-    <TopBar />
+      <TopBar />
+      <Routes>
+      <Route path="/" element={<Dashboard />} />
+      {/* <Route path="/team" element={<Team />} /> */}
+      {/* <Route path="/contacts" element={<Contacts />} /> */}
+      {/* <Route path="/invoices" element={<Invoices />} /> */}
+      {/* <Route path="/form" element={<Form />} /> */}
+      {/* <Route path="/bar" element={<Bar />} /> */}
+      {/* <Route path="/pie" element={<Pie />} /> */}
+      {/* <Route path="/line" element={<Line />} /> */}
+      {/* <Route path="/faq" element={<FAQ />} /> */}
+      {/* <Route path="/calendar" element={<Calendar />} /> */}
+      {/* <Route path="/geography" element={<Geography />} /> */}
+        
+      </Routes>
 
 
     </main>
