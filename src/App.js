c4e98fca2a0c9f3c806d66735c194e392e2a8eb8
wandieinnocents <wandieinnocent2@gmail.com>
2023-10-24 11:24:36 +0300

    Refactor code,

--- src/App.js
+++ src/App.js
@@ -35,29 +35,31 @@ function App() {
 
     <div className="app"> 
 
-    <SideBar />
-    {/* Main content */}
-    <main className="content">
-      <TopBar />
-      <Routes>
-      <Route path="/" element={<Dashboard />} />
-      {/* <Route path="/team" element={<Team />} /> */}
-      {/* <Route path="/contacts" element={<Contacts />} /> */}
-      {/* <Route path="/invoices" element={<Invoices />} /> */}
-      {/* <Route path="/form" element={<Form />} /> */}
-      {/* <Route path="/bar" element={<Bar />} /> */}
-      {/* <Route path="/pie" element={<Pie />} /> */}
-      {/* <Route path="/line" element={<Line />} /> */}
-      {/* <Route path="/faq" element={<FAQ />} /> */}
-      {/* <Route path="/calendar" element={<Calendar />} /> */}
-      {/* <Route path="/geography" element={<Geography />} /> */}
-        
-      </Routes>
+      {/* Side bar */}
+      <SideBar />
+      
+      {/* Main content */}
+      <main className="content">
+        <TopBar />
+        <Routes>
+        <Route path="/" element={<Dashboard />} />
+        {/* <Route path="/team" element={<Team />} /> */}
+        {/* <Route path="/contacts" element={<Contacts />} /> */}
+        {/* <Route path="/invoices" element={<Invoices />} /> */}
+        {/* <Route path="/form" element={<Form />} /> */}
+        {/* <Route path="/bar" element={<Bar />} /> */}
+        {/* <Route path="/pie" element={<Pie />} /> */}
+        {/* <Route path="/line" element={<Line />} /> */}
+        {/* <Route path="/faq" element={<FAQ />} /> */}
+        {/* <Route path="/calendar" element={<Calendar />} /> */}
+        {/* <Route path="/geography" element={<Geography />} /> */}
+          
+        </Routes>
 
 
-    </main>
+      </main>
 
-    {/* End main content */}
+      {/* End main content */}
     
     </div>
     </ThemeProvider>
