e477a74b1f76757e6b4a14c733395d0e8440eb87
wandieinnocents <wandieinnocent2@gmail.com>
2023-11-06 11:26:02 +0300

    Add bar chart

---
+++ src/pages/ChartPages/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ChartPages() {
+  return (
+    <div>
+      ChartPages
+    </div>
+  );
+}
+
+export default ChartPages;
--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from '../../components/Header';
 import { Box, Button, IconButton, Typography, useTheme } from "@mui/material";
+import BarChart from '../../components/Charts/BarChart';
 
 const Dashboard = () => {
   return (
@@ -10,6 +11,10 @@ const Dashboard = () => {
       <Header title="DASHBOARD" subtitle="Welcome to your dashboard" />
 
       <p>Add content of dashboard here</p>
+      <Box height="75vh">
+      <BarChart />
+      </Box>
+
       </Box>
     </Box>
   );
